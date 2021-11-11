6f4e59a364c9328f610728f8aa7bd4cda3c8d1ff
shentuye <shen@ifkasolution.com>
2021-11-11 21:07:27 +0100

    done

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var principal = document.getElementById("principal").value;
+  if (principal<=0) {
+    alert("Enter a positive number")
+    document.getElementById('principal').focus();
+  } else {
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    document.getElementById("result").innerHTML = "<p>If you deposit <mark>" + principal +
+      "</mark>,<br>at an interest rate of <mark>" + rate +
+      "%</mark>,<br>You will receive an amount of <mark>" + interest +
+      "</mark>,<br>in the year <mark>" + year + "</mark></p>";
+  }
+
 }
-        
+
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + "%";
+}
